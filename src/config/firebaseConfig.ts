96e023d1c33178d40a3864a587553443fd62dfff
Hazley Jarquin <hazleyjarquin03@gmail.com>
2024-10-21 00:17:35 -0600

    Update firebaseConfig to use environment variables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBK0UsI9s080O0Arg5Az1_W6925gTn9I4Y",
-  authDomain: "luired-4266c.firebaseapp.com",
-  projectId: "luired-4266c",
-  storageBucket: "luired-4266c.appspot.com",
-  messagingSenderId: "995506588899",
-  appId: "1:995506588899:web:4e6bee8c25c6f3f32bc9d6",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
